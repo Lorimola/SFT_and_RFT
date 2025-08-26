82ccdc4d0df98ff12b3c690e319afd4e7b0b795a
lingzhong <lingzhong@sjtu.edu.cn>
2025-08-26 11:40:16 +0800

    update

--- Code/run.sh
+++ Code/run.sh
@@ -16,16 +16,16 @@ echo "script.py finish"
 
 
 echo "Running train_sft.py"
-python3 /data2/home/donglingzhong/yangsb/SAR/Code/train_sft.py --model_name_or_path=/data4/models_wzr/Qwen2.5-VL-7B-Instruct --dataset_path=/data2/home/donglingzhong/yangsb/Dateset/Train/AITZ --output_dir=/data2/home/donglingzhong/yangsb/Models/SFT/AITZ --num_train_epochs=3 --learning_rate=1e-5 --bf16=True --use_lora=True
+python3 /data2/home/donglingzhong/yangsb/SAR/Code/train_sft.py --model_name_or_path=/data4/models_wzr/Qwen2.5-VL-7B-Instruct --dataset_path=/data2/home/donglingzhong/yangsb/Dateset/Train/AITZ --output_dir=/data2/home/donglingzhong/yangsb/Models/SFT/AITZ --num_train_epochs=3 --learning_rate=1e-5 --bf16=True --use_lora=False
 
-python3 /data2/home/donglingzhong/yangsb/SAR/Code/train_sft.py --model_name_or_path=/data4/models_wzr/Qwen2.5-VL-7B-Instruct --dataset_path=/data2/home/donglingzhong/yangsb/Dateset/Train/android_control --output_dir=/data2/home/donglingzhong/yangsb/Models/SFT/android_control --num_train_epochs=3 --learning_rate=1e-5 --bf16=True --use_lora=True
+python3 /data2/home/donglingzhong/yangsb/SAR/Code/train_sft.py --model_name_or_path=/data4/models_wzr/Qwen2.5-VL-7B-Instruct --dataset_path=/data2/home/donglingzhong/yangsb/Dateset/Train/android_control --output_dir=/data2/home/donglingzhong/yangsb/Models/SFT/android_control --num_train_epochs=3 --learning_rate=1e-5 --bf16=True --use_lora=False
 echo "train_sft.py finish"
 
 
 echo "Running train_rft.py"
-python3 /data2/home/donglingzhong/yangsb/SAR/Code/train_rft.py --model_name_or_path=/data4/models_wzr/Qwen2.5-VL-7B-Instruct --dataset_path=/data2/home/donglingzhong/yangsb/Dateset/Train/AITZ --output_dir=/data2/home/donglingzhong/yangsb/Models/RFT/AITZ --learning_rate=1e-6 --num_train_epochs=3 --bf16=True --use_qlora=True
+python3 /data2/home/donglingzhong/yangsb/SAR/Code/train_rft.py --model_name_or_path=/data4/models_wzr/Qwen2.5-VL-7B-Instruct --dataset_path=/data2/home/donglingzhong/yangsb/Dateset/Train/AITZ --output_dir=/data2/home/donglingzhong/yangsb/Models/RFT/AITZ --learning_rate=1e-6 --num_train_epochs=3 --bf16=True --use_qlora=False
 
-python3 /data2/home/donglingzhong/yangsb/SAR/Code/train_rft.py --model_name_or_path=/data4/models_wzr/Qwen2.5-VL-7B-Instruct --dataset_path=/data2/home/donglingzhong/yangsb/Dateset/Train/android_control --output_dir=/data2/home/donglingzhong/yangsb/Models/RFT/android_control --learning_rate=1e-6 --num_train_epochs=3 --bf16=True --use_qlora=True
+python3 /data2/home/donglingzhong/yangsb/SAR/Code/train_rft.py --model_name_or_path=/data4/models_wzr/Qwen2.5-VL-7B-Instruct --dataset_path=/data2/home/donglingzhong/yangsb/Dateset/Train/android_control --output_dir=/data2/home/donglingzhong/yangsb/Models/RFT/android_control --learning_rate=1e-6 --num_train_epochs=3 --bf16=True --use_qlora=False
 echo "train_rft.py finish"
 
 
